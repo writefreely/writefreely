1274914207af7b5ff56635572ee55ee7dc916fe5
Matt Baer <matt@write.as>
2018-12-24 12:45:15 -0500

    Add copyright / license notices to .go files

--- webfinger.go
+++ webfinger.go
@@ -1,3 +1,12 @@
+/*
+ * Copyright © 2018 A Bunch Tell LLC.
+ *
+ * This file is part of WriteFreely.
+ *
+ * WriteFreely is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, included
+ * in the LICENSE file in this source code package.
+ */
 package writefreely
 
 import (
