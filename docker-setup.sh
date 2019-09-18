42f6733980db896b21a4283b1b8a6d196f96ac0f
Rob Loranger <dev@loranger.xyz>
2019-09-18 08:13:29 -0700

    remove containers after use

--- docker-setup.sh
+++ docker-setup.sh
@@ -3,7 +3,7 @@
 # for a writefreely instance using the docker-compose.yml in this repo
 
 # start database
-docker run -d --volume=writefreely_db-data:/var/lib/mysql \
+docker run --rm -d --volume=writefreely_db-data:/var/lib/mysql \
 --name=db \
 -e "MYSQL_DATABASE=writefreely" \
 -e "MYSQL_ROOT_PASSWORD=changeme" \
@@ -11,13 +11,12 @@ docker run -d --volume=writefreely_db-data:/var/lib/mysql \
 mariadb:latest 
 
 # create new asset signing keys
-docker run --volume=writefreely_web-data:/home/writefreely writeas/writefreely:latest "-gen-keys"
+docker run --rm --volume=writefreely_web-data:/home/writefreely writeas/writefreely:latest "-gen-keys"
 
 # generate new configuration and initialize database
-docker run -it --volume=writefreely_web-data:/home/writefreely \
+docker run --rm -it --volume=writefreely_web-data:/home/writefreely \
 --link db:db \
 writeas/writefreely:latest "-config"
 
 # clean up detached database container
 docker container stop db
-docker container rm db
