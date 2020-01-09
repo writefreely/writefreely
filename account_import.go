5b7f37aed8f70273c8d5fcd49abb6d4507476b86
Matt Baer <matt@write.as>
2020-01-09 11:16:26 -0500

    Restyle Import page
    - Changes Import link location in dropdown menu
    - Makes design consistent with Invite People page (and extracts some
      common CSS into core.less)
    - Selects the user's first blog by default in the dropdown
    - Changes the copy a bit
    
    Ref T609

--- account_import.go
+++ account_import.go
@@ -18,7 +18,7 @@ import (
 
 func viewImport(app *App, u *User, w http.ResponseWriter, r *http.Request) error {
 	// Fetch extra user data
-	p := NewUserPage(app, r, u, "Import", nil)
+	p := NewUserPage(app, r, u, "Import Posts", nil)
 
 	c, err := app.db.GetCollections(u, app.Config().App.Host)
 	if err != nil {
