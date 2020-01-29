d6b7a5925f20ba7ca3d9f210188a3f1ceba07cf1
Matt Baer <matt@write.as>
2020-01-29 09:11:02 -0500

    Restrict /invite/{code} route to valid chars
    Previously, loading something like /invite/fFdblk😄 would return a 500,
    due to a mix of collations in MySQL while SELECTing for an invite with
    an ID of 'fFdblk😄'. This restricts the route to [a-zA-Z0-9] chars, to
    prevent this.

--- routes.go
+++ routes.go
@@ -161,7 +161,7 @@ func InitRoutes(apper Apper, r *mux.Router) *mux.Router {
 	// Handle special pages first
 	write.HandleFunc("/login", handler.Web(viewLogin, UserLevelNoneRequired))
 	write.HandleFunc("/signup", handler.Web(handleViewLanding, UserLevelNoneRequired))
-	write.HandleFunc("/invite/{code}", handler.Web(handleViewInvite, UserLevelOptional)).Methods("GET")
+	write.HandleFunc("/invite/{code:[a-zA-Z0-9]+}", handler.Web(handleViewInvite, UserLevelOptional)).Methods("GET")
 	// TODO: show a reader-specific 404 page if the function is disabled
 	write.HandleFunc("/read", handler.Web(viewLocalTimeline, UserLevelReader))
 	RouteRead(handler, UserLevelReader, write.PathPrefix("/read").Subrouter())
