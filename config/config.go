b58464addbe0be591c257f244e8bc5f53f15d89d
Matt Baer <matt@write.as>
2020-02-14 14:08:53 -0500

    Optionally hide Monitor page in Admin nav
    This adds a new config option that signifies the admin doesn't need to
    see deeply technical things, like application resource usage. In the
    [app] section, set forest = true to enable this.
    
    Ref T694

--- config/config.go
+++ config/config.go
@@ -93,6 +93,7 @@ type (
 
 		// Site functionality
 		Chorus        bool `ini:"chorus"`
+		Forest        bool `ini:"forest"` // The admin cares about the forest, not the trees. Hide unnecessary technical info.
 		DisableDrafts bool `ini:"disable_drafts"`
 
 		// Users
