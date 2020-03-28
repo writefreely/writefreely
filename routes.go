c1643ab01df532df4d34947e938fa0c1fa5bfaf5
Keturah Dola-Borg <keturah@mania.systems>
2020-03-27 22:18:52 -0700

    Apply go fmt

--- routes.go
+++ routes.go
@@ -76,7 +76,7 @@ func InitRoutes(apper Apper, r *mux.Router) *mux.Router {
 	configureSlackOauth(handler, write, apper.App())
 	configureWriteAsOauth(handler, write, apper.App())
 	configureGitlabOauth(handler, write, apper.App())
-    configureGenericOauth(handler, write, apper.App())
+	configureGenericOauth(handler, write, apper.App())
 
 	// Set up dyamic page handlers
 	// Handle auth
