339646f4c4d1db552aee85dbf8c7525be4360915
Leo <i@hardrain980.com>
2025-07-12 17:30:45 +0800

    db/alter.go, db/create.go db/dialect.go: wrap for postgres dialect
    Signed-off-by: Leo <i@hardrain980.com>

--- db/alter.go
+++ db/alter.go
@@ -5,48 +5,69 @@ import (
 	"strings"
 )
 
+type alterTableQueryType int
+
+const (
+	alter  alterTableQueryType = iota
+	update alterTableQueryType = iota
+)
+
+type change struct {
+	queryType   alterTableQueryType
+	queryString string
+}
+
 type AlterTableSqlBuilder struct {
 	Dialect DialectType
 	Name    string
-	Changes []string
+	Changes []change
 }
 
 func (b *AlterTableSqlBuilder) AddColumn(col *Column) *AlterTableSqlBuilder {
 	if colVal, err := col.String(); err == nil {
-		b.Changes = append(b.Changes, fmt.Sprintf("ADD COLUMN %s", colVal))
+		b.Changes = append(b.Changes, change{queryType: alter, queryString: fmt.Sprintf("ADD COLUMN %s", colVal)})
 	}
 	return b
 }
 
 func (b *AlterTableSqlBuilder) ChangeColumn(name string, col *Column) *AlterTableSqlBuilder {
 	if colVal, err := col.String(); err == nil {
-		b.Changes = append(b.Changes, fmt.Sprintf("CHANGE COLUMN %s %s", name, colVal))
+		b.Changes = append(b.Changes, change{queryType: alter, queryString: fmt.Sprintf("RENAME COLUMN %s TO %s_old", name, name)})
+		b.Changes = append(b.Changes, change{queryType: alter, queryString: fmt.Sprintf("ADD COLUMN %s", colVal)})
+		b.Changes = append(b.Changes, change{queryType: update, queryString: fmt.Sprintf("SET %s = %s_old", col.Name, name)})
+		b.Changes = append(b.Changes, change{queryType: alter, queryString: fmt.Sprintf("DROP COLUMN %s", name)})
 	}
 	return b
 }
 
 func (b *AlterTableSqlBuilder) AddUniqueConstraint(name string, columns ...string) *AlterTableSqlBuilder {
-	b.Changes = append(b.Changes, fmt.Sprintf("ADD CONSTRAINT %s UNIQUE (%s)", name, strings.Join(columns, ", ")))
+	b.Changes = append(b.Changes, change{
+		queryType: alter,
+		queryString: fmt.Sprintf("ADD CONSTRAINT %s UNIQUE (%s)", name, strings.Join(columns, ", ")),
+	})
 	return b
 }
 
 func (b *AlterTableSqlBuilder) ToSQL() (string, error) {
-	var str strings.Builder
+	str := ""
+	changeCount := len(b.Changes)
 
-	str.WriteString("ALTER TABLE ")
-	str.WriteString(b.Name)
-	str.WriteString(" ")
-
-	if len(b.Changes) == 0 {
+	if changeCount == 0 {
 		return "", fmt.Errorf("no changes provide for table: %s", b.Name)
 	}
-	changeCount := len(b.Changes)
-	for i, thing := range b.Changes {
-		str.WriteString(thing)
-		if i < changeCount-1 {
-			str.WriteString(", ")
+
+	for i, change := range b.Changes {
+		switch change.queryType {
+		case alter:
+			str += fmt.Sprintf("ALTER TABLE %s ", b.Name)
+		case update:
+			str += fmt.Sprintf("UPDATE %s ", b.Name)
+		}
+		str += change.queryString
+		if i < changeCount - 1 {
+			str += "; "
 		}
 	}
 
-	return str.String(), nil
+	return str, nil
 }
--- db/dialect.go
+++ db/dialect.go
@@ -5,8 +5,9 @@ import "fmt"
 type DialectType int
 
 const (
-	DialectSQLite DialectType = iota
-	DialectMySQL  DialectType = iota
+	DialectSQLite   DialectType = iota
+	DialectMySQL    DialectType = iota
+	DialectPostgres DialectType = iota
 )
 
 func (d DialectType) Column(name string, t ColumnType, size OptionalInt) *Column {
@@ -15,6 +16,8 @@ func (d DialectType) Column(name string, t ColumnType, size OptionalInt) *Column
 		return &Column{Dialect: DialectSQLite, Name: name, Type: t, Size: size}
 	case DialectMySQL:
 		return &Column{Dialect: DialectMySQL, Name: name, Type: t, Size: size}
+	case DialectPostgres:
+		return &Column{Dialect: DialectPostgres, Name: name, Type: t, Size: size}
 	default:
 		panic(fmt.Sprintf("unexpected dialect: %d", d))
 	}
@@ -26,6 +29,8 @@ func (d DialectType) Table(name string) *CreateTableSqlBuilder {
 		return &CreateTableSqlBuilder{Dialect: DialectSQLite, Name: name}
 	case DialectMySQL:
 		return &CreateTableSqlBuilder{Dialect: DialectMySQL, Name: name}
+	case DialectPostgres:
+		return &CreateTableSqlBuilder{Dialect: DialectPostgres, Name: name}
 	default:
 		panic(fmt.Sprintf("unexpected dialect: %d", d))
 	}
@@ -37,6 +42,8 @@ func (d DialectType) AlterTable(name string) *AlterTableSqlBuilder {
 		return &AlterTableSqlBuilder{Dialect: DialectSQLite, Name: name}
 	case DialectMySQL:
 		return &AlterTableSqlBuilder{Dialect: DialectMySQL, Name: name}
+	case DialectPostgres:
+		return &AlterTableSqlBuilder{Dialect: DialectPostgres, Name: name}
 	default:
 		panic(fmt.Sprintf("unexpected dialect: %d", d))
 	}
@@ -48,6 +55,8 @@ func (d DialectType) CreateUniqueIndex(name, table string, columns ...string) *C
 		return &CreateIndexSqlBuilder{Dialect: DialectSQLite, Name: name, Table: table, Unique: true, Columns: columns}
 	case DialectMySQL:
 		return &CreateIndexSqlBuilder{Dialect: DialectMySQL, Name: name, Table: table, Unique: true, Columns: columns}
+	case DialectPostgres:
+		return &CreateIndexSqlBuilder{Dialect: DialectPostgres, Name: name, Table: table, Unique: true, Columns: columns}
 	default:
 		panic(fmt.Sprintf("unexpected dialect: %d", d))
 	}
@@ -59,6 +68,8 @@ func (d DialectType) CreateIndex(name, table string, columns ...string) *CreateI
 		return &CreateIndexSqlBuilder{Dialect: DialectSQLite, Name: name, Table: table, Unique: false, Columns: columns}
 	case DialectMySQL:
 		return &CreateIndexSqlBuilder{Dialect: DialectMySQL, Name: name, Table: table, Unique: false, Columns: columns}
+	case DialectPostgres:
+		return &CreateIndexSqlBuilder{Dialect: DialectPostgres, Name: name, Table: table, Unique: false, Columns: columns}
 	default:
 		panic(fmt.Sprintf("unexpected dialect: %d", d))
 	}
@@ -70,6 +81,8 @@ func (d DialectType) DropIndex(name, table string) *DropIndexSqlBuilder {
 		return &DropIndexSqlBuilder{Dialect: DialectSQLite, Name: name, Table: table}
 	case DialectMySQL:
 		return &DropIndexSqlBuilder{Dialect: DialectMySQL, Name: name, Table: table}
+	case DialectPostgres:
+		return &DropIndexSqlBuilder{Dialect: DialectPostgres, Name: name, Table: table}
 	default:
 		panic(fmt.Sprintf("unexpected dialect: %d", d))
 	}
