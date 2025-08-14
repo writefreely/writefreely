52aee878a3ecdd9687f0b5d262e4e4dd128cf95b
Matt Baer <matt@write.as>
2025-08-14 11:50:55 -0400

    Fix Like counts on Stats page
    This adds a missing query to populate each post with the number of likes it's received.

--- database.go
+++ database.go
@@ -2075,6 +2075,12 @@ func (db *datastore) GetTopPosts(u *User, alias string, hostName string) (*[]Pub
 			c.hostName = hostName
 			pubPost.Collection = &CollectionObj{Collection: c}
 		}
+		p.LikeCount, err = db.GetPostLikeCounts(p.ID)
+		if err != nil {
+			log.Error("Failed GetPostLikeCounts(%s): %v", p.ID, err)
+			gotErr = true
+			break
+		}
 
 		posts = append(posts, pubPost)
 	}
